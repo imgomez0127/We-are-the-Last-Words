29ff0c4d3cef89e1ac748a00b2fa7e6ed08dcb66
SadP4nda <ianm0127@hotmail.com>
2018-01-24 21:04:48 -0500

    .

--- larablog/wsgi.py
+++ larablog/wsgi.py
@@ -1,16 +1,14 @@
 """
-WSGI config for larablog project.
-
+WSGI config for {{ project_name }} project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "larablog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
 application = get_wsgi_application()
